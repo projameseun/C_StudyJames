67962cfd3a50d59b0f4807fab197b819c6b20a69
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2024-08-05 20:54:47 +0900

    iterator
    이터레이터 begin,end,operator

--- 44.Iterator/44.Iterator/CArray.h
+++ 44.Iterator/44.Iterator/CArray.h
@@ -32,45 +32,78 @@ public:
 		return m_pData;
 	}
 
-	
+
 public:
 	//앞으로 내가만들 T는 무슨 타입이든 다되 
 	void push_back(const T& _Data);
 	void resize(int _Size);
 	void Delete(int _Num);
-	
+
 
 	T& operator[] (int _idx);
 	//int operator[] (int _idx);
 
 	class iterator; //전방선언
 	iterator begin();
+	iterator end();
 
 public:
 	//이너클래스
 	//반복자
+	//이너클래스는 포함하고 있는 클래스의 pirivate멤버 접근가능
 	class iterator
 	{
 	private:
-		T*		m_pData;
-		int		m_iIdx;
+		//백터가 추가됬을때 복사가 일어나서 새로할당될때텐데 이때 문제가 생길수 있다 
+		//iterator는 결국 백터객체자신을 알고 잇으면된다 그러면 새로 할당되고 똑같이 그주소를 가리켜주면 되니까 
+		//설계적으로 봤을때 백터자기자신이 크기가 변경되어 첫번째 주소를 가리키고 , iter가 가리키는 첫번째주소를 가리키는게 서로 다르는게 문제라고 만들거라면 두개 다 있어야 된다.
+		//실제 백터는 이걸 문제로 보고있다.
+			CArray*		m_pArr;	//iterator가 가리킬 데이터를 관리하는 가변배열주소
+			T*		m_pData;	//데이터 시작주소
+			int		m_iIdx;		//가리키는 인덱스
 	public:
 		iterator() :
-			m_pData	(nullptr),
+			m_pArr(nullptr),
+			m_pData(nullptr),
 			m_iIdx(-1)
 		{
-			
+
 		}
 
-		iterator(T _data, int _idx) :
+		iterator(CArray* _pArr ,T* _data, int _idx) :
+			m_pArr(_pArr),
 			m_pData(_data),
 			m_iIdx(-_idx)
 		{
-
+			
 		}
 		~iterator()
 		{
-		
+
+		}
+
+	public:
+		T& operator * ()
+		{
+			//비교 iterator가 알고 있는 주소와, 가변배열이 알고 있는 주소가 달라진경우(공간을 확장하고 주소가 달라질 경우)
+			//iterator end iterator일 경우
+			if (m_pArr->m_pData != m_pData || m_iIdx == -1)
+			{
+				std::cout << "can't dereference value-initialized vector iterator" << std::endl;
+				assert(nullptr);
+			}
+
+			return m_pData[m_iIdx];
+		}
+
+		iterator& operator++()
+		{
+			return *this;
+		}
+
+		iterator& operator --()
+		{
+			return *this;
 		}
 
 	};
@@ -210,9 +243,25 @@ typename CArray<T>::iterator CArray<T>::begin()
 	//iterator iter(m_pData, 0);
 	/*iter.m_pData = this->m_pData;
 	iter.m_iIdx = 0;*/
-	
+
+	if (m_iCount == 0)
+	{
+		return iterator(this, m_pData, -1);	//데이터가 없는경우 begin() == end()
+	}
+
 	//임시객체
-	return iterator(m_pData,0);
+	return iterator(this,m_pData, 0);
+}
+
+
+template<class T>
+typename CArray<T>::iterator CArray<T>::end()
+{
+	
+	//끝의 다음을 가리키는 iteator를 만들어서 반환
+	return iterator(this, m_pData, -1);
+
+
 }
 
 
