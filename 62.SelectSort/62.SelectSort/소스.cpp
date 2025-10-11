d420c77cc5e0e92b7082df8dde64b7621dd2d8fc
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2025-10-11 23:46:59 +0900

    select sort
    select sort

--- 62.SelectSort/62.SelectSort/소스.cpp
+++ 62.SelectSort/62.SelectSort/소스.cpp
@@ -4,28 +4,29 @@
 
 using namespace std;
 
-vector<int> SelectSort(vector<int> data)
-{
-	for (int i = 0; i < data.size(); ++i)
-	{
-		
 
+
+vector<int> SelectSort(vector<int> data)
+{	
+	//data.size() -1 를 한 이유는 마지막 인덱스는 이미 정렬되어 있기때문에 굳이 하지않는다.
+	for (int i = 0; i < data.size()-1 ; ++i)
+	{
+		int minIdx = i;
+		
 		for (int j = i + 1; j < data.size(); ++j)
 		{
-			int iTemp = 0;
-			if (data[i] > data[j])
-			{
-	
-				iTemp = data[i];
-				data[i] = data[j];
-				data[j] = iTemp;
-							
-			}
+			if (data[j] < data[minIdx])
+				minIdx = j;
+		}
+		if (minIdx != i)
+		{
+			int iTemp = data[i];
+			data[i] = data[minIdx];
+			data[minIdx] = iTemp;
+			//swap(data[i], data[minIdx]);
 		}
 	}
-
 	return data;
-	
 }
 
 
